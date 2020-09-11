643da13dc10ea9aad184dd9fc70a4fefaf6bb2d5
Min Chen <chenm003@163.com>
2020-09-12 00:36:00 +0800

    [x266] command line parser

--- src/x266.cpp
+++ src/x266.cpp
@@ -22,9 +22,72 @@
  * For more information, contact us at chenm003@163.com.
  *****************************************************************************/
 
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
+    if(argc < 5)
+    {
+        fprintf(stderr, "Usage: %s -i in_file -o out_file -w width -h height -f frames\n", argv[0]);
+        return 0;
+    }
+
+    // Encoder parameters
+    FILE *fpi = NULL;
+    FILE *fpo = NULL;
+    int nWidth = 0;
+    int nHeight = 0;
+    int nFrames = 0;
+
+    int i;
+    for(i = 1; i < argc; i++)
+    {
+        if(!_stricmp(argv[i], "-i"))
+        {
+            fpi = fopen(argv[++i], "rb");
+        }
+        else if(!_stricmp(argv[i], "-o"))
+        {
+            fpo = fopen(argv[++i], "wb");
+        }
+        else if(!_stricmp(argv[i], "-w"))
+        {
+            nWidth = atoi(argv[++i]);
+        }
+        else if(!_stricmp(argv[i], "-h"))
+        {
+            nHeight = atoi(argv[++i]);
+        }
+        else if(!_stricmp(argv[i], "-f"))
+        {
+            nFrames = atoi(argv[++i]);
+        }
+    }
+
+    // Validate parameters
+    if(nWidth == 0 ||
+       nHeight == 0 ||
+       fpi == NULL ||
+       fpo == NULL)
+    {
+        fprintf(stderr, "Parameters check failed\n");
+
+        if(fpi)
+            fclose(fpi);
+        if(fpo)
+            fclose(fpo);
+        return -1;
+    }
+
+    // Encode
+
+    // Cleanup
+    fclose(fpi);
+    fclose(fpo);
+
     return 0;
 }
 
