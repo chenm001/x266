b489c8c96b3e1c7d0d3496d8dd91ccf9f9afd16b
Min Chen <chenm003@163.com>
2020-09-11 23:25:35 +0800

    [x266] Initialize import and roadmap   (a) The code write during unemployment / unpaid leave period, it develop       on my personal laptop, I think all of code copyright is own by me.
      (b) Based on (a), the development time and budget are very limited, so I
          have to put my eye on architecture design,
          as a rssult, the early preview will be functionally, but the code
          style are NOT good enough, I will refactor in future.
    
      (c.1) The Encoder source code write as a standalone x266.cpp, it helpful
            optimized with compiler and simplify code works on dynamic
            configurable function pointer.
    
      (c.2) The Encoder primary target to mobile device, my goal is compare to
            the x264, the speed performance less than 1.4x and compression
            ratio more than 1.3x.
    
      (c.3) There will be a series plugins interface, such fast mode decide,
            early termination, GPU, task queue, etc.
    
      (c.4) I will support RDO framework but limited, because mobile device
            need more trade-off in between compression and speed performance,
            I will try to find a balance over there.
    
      (End) Sorry for my terribe English.

---
+++ src/x266.cpp
@@ -0,0 +1,30 @@
+/*****************************************************************************
+ * x266.cpp: WC Encoder Functions
+ *****************************************************************************
+ * Copyright (C) 2015-2020 x266 project
+ *
+ * Authors: Min Chen <chenm003@163.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at chenm003@163.com.
+ *****************************************************************************/
+
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
+
