422a5a2a2481b407a6396ae9ff0aad88dbaddc58
Min Chen <chenm003@163.com>
2020-09-12 23:07:30 +0800

    [x266] xConvFmt for convert input YUV420 into internal frame data struct

--- src/x266.cpp
+++ src/x266.cpp
@@ -27,6 +27,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <assert.h>
 
  /*****************************************************************************
  *****************************************************************************/
@@ -49,24 +50,59 @@ PACKED(struct _ref_block_t
 {
     // REF_BLOCK_SZ=16
     uint8_t     m_Y[16*16];         // 256 bytes - Y
-    uint8_t     m_C[2][8*8];        // 128 bytes - UV
+    uint8_t     m_C[2*8*8];         // 128 bytes - UV
     uint8_t     m_I[128];           // 128 bytes - Info
 });
 typedef struct _ref_block_t ref_block_t;
 
-PACKED(struct _ref_frame_t
-{
-    ref_block_t     m_frame[(MAX_WIDTH/REF_BLOCK_SZ) * (MAX_HEIGHT/REF_BLOCK_SZ)];
-});
-typedef struct _ref_frame_t ref_frame_t;
-
 typedef struct _codec_t
 {
-    ref_frame_t     m_frames[3];    // [0]=Cur, [1..N]=References
-
+    ref_block_t    *m_frames[3];            // [0]=Cur, [1..N]=References
+    intptr_t        m_frames_strd;
 } codec_t;
 
 /*****************************************************************************
+ *****************************************************************************/
+void xConvInputFmt(ref_block_t     *pBlock,
+                   const uint8_t   *inpY,
+                   const intptr_t   strdY,
+                   const uint8_t   *inpU,
+                   const uint8_t   *inpV,
+                   const intptr_t   strdC,
+                   const int        width,
+                   const int        height)
+{
+    assert(!(width % REF_BLOCK_SZ) && "width is not multiple of 16");
+    assert(!(height % REF_BLOCK_SZ) && "height is not multiple of 16");
+
+    int i, j, x, y;
+    for(y = 0; y < height; y += REF_BLOCK_SZ)
+    {
+        for(x = 0; x < width; x += REF_BLOCK_SZ)
+        {
+            // Y
+            for(i = 0; i < REF_BLOCK_SZ; i++)
+            {
+                memcpy(&pBlock->m_Y[i * REF_BLOCK_SZ],
+                       &inpY[(y+i)*strdY + x],
+                       REF_BLOCK_SZ*sizeof(uint8_t));
+            }
+
+            // UV - 420SP
+            for(i = 0; i < REF_BLOCK_SZ/2; i++)
+            {
+                for(j = 0; j < REF_BLOCK_SZ/2; j++)
+                {
+                    pBlock->m_C[i*REF_BLOCK_SZ + j*2 + 0] = inpU[((y>>1)+i)*strdC + ((x>>1)+j)];
+                    pBlock->m_C[i*REF_BLOCK_SZ + j*2 + 1] = inpV[((y>>1)+i)*strdC + ((x>>1)+j)];
+                }
+            }
+            pBlock++;
+        }
+    }
+}
+
+ /*****************************************************************************
  *****************************************************************************/
 int main(int argc, char *argv[])
 {
@@ -108,27 +144,49 @@ int main(int argc, char *argv[])
         }
     }
 
+    int ret = 0;
+
     // Validate parameters
-    if(nWidth == 0 ||
-       nHeight == 0 ||
-       fpi == NULL ||
-       fpo == NULL)
+    if(nWidth == 0 || nWidth > MAX_WIDTH ||
+       nHeight == 0 || nHeight > MAX_HEIGHT ||
+       fpi == NULL || fpo == NULL)
     {
         fprintf(stderr, "Parameters check failed\n");
-
-        if(fpi)
-            fclose(fpi);
-        if(fpo)
-            fclose(fpo);
-        return -1;
+        ret = -1;
+        goto _cleanup;
     }
+
+    // Unit Tests
+#if TEST_xConvInputFmt
+    {
+        int i, j;
+        uint8_t tmp[32*16+2*32*16/4];
+        memset(tmp, 0xCC, sizeof(tmp));
+
+        for(i = 0; i < 32*16+2*32*16/4; i++)
+        {
+            tmp[i] = i;
+        }
+
+        ref_block_t blocks[2];
+        memset(blocks, 0xCD, sizeof(blocks));
+
+        xConvInputFmt(blocks, &tmp[0], 32, &tmp[256], &tmp[320], 16, 32, 16);
+        printf("xConvFmt Done\n");
+    }
+#endif
 
     // Encode
 
-    // Cleanup
-    fclose(fpi);
-    fclose(fpo);
+    // Prepare input frame
 
-    return 0;
+    // Cleanup
+_cleanup:
+    if(fpi)
+        fclose(fpi);
+    if(fpo)
+        fclose(fpo);
+
+    return ret;
 }
 
