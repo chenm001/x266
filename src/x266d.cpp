24d2c87ac6fb1272a5b42b80674511deb38e36d8
Min Chen <chenm003@163.com>
2020-10-05 02:29:41 +0800

    [x266] Decode VPS and adjust interface on bitstream operators

--- src/x266d.cpp
+++ src/x266d.cpp
@@ -31,8 +31,14 @@
 
 /*****************************************************************************
  *****************************************************************************/
-#define BIT_BUF_SIZE        (1 * 1024 * 1024)
-#define REF_BLOCK_SZ        (16)
+#define BIT_BUF_SIZE                    (1 * 1024 * 1024)
+#define REF_BLOCK_SZ                    (16)
+#define MAX_VPS_LAYERS                  (1)     // maximum u(6)
+#define MAX_VPS_SUB_LAYERS              (1)     // maximum u(3)
+#define MAX_VPS_OUTPUT_LAYER            (2)     // maximum u(8)
+#define MAX_VPS_PTLS                    (1)     // maximum u(8)
+#define MAX_VPS_DPB_PARAMS              (1)     // maximum ue(v)
+#define MAX_VPS_HRD_PARAMS              (1)     // maximum ue(v)
 
 // Thanks to https://gist.github.com/PhilCK/1534763
 #ifdef __GNUC__
@@ -53,11 +59,26 @@
     #define BSWAP32(x)      __builtin_bswap32(x)
     #define BSWAP64(x)      __builtin_bswap64(x)
     #define CLZ(x)          (__builtin_clz(x) ^ 31)
+    #define CTZ(x)          __builtin_ctz(x)
+
 #elif defined(_MSC_VER)
     #include <intrin.h>
     #define BSWAP32(x)      _byteswap_ulong(x)
     #define BSWAP64(x)      _byteswap_uint64(x)
-    #define CLZ(x)          __lzcnt(x)
+
+    static uint32_t __inline CLZ(uint32_t x)
+    {
+       unsigned long r = 0;
+       _BitScanReverse(&r, x);
+       return r;
+    }
+
+    static uint32_t __inline CTZ(uint32_t x)
+    {
+       unsigned long r = 0;
+       _BitScanForward(&r, x);
+       return r;
+    }
 #else
     #error Unsupport platform
 #endif
@@ -83,10 +104,10 @@ typedef struct _param_t param_t;
 
 PACKED(struct _bs_t
 {
-    uint8_t    *pucBuf;
-    uint8_t    *pucBufEnd;
     uint64_t    dwCache;
+    uint8_t    *pucBuf;
     int32_t     cachedBits;
+    int32_t     rbspBits;
 });
 typedef _bs_t bs_t;
 
@@ -112,6 +133,41 @@ PACKED(struct _nal_unit_header_t
 });
 typedef _nal_unit_header_t nal_unit_header_t;
 
+typedef struct _vps_t
+{
+    uint8_t     vps_video_parameter_set_id;                                             // u(4)
+    uint8_t     vps_max_layers_minus1;                                                  // u(6)
+    uint8_t     vps_max_sublayers_minus1;                                               // u(3)
+    uint8_t     vps_default_ptl_dpb_hrd_max_tid_flag;                                   // u(1)
+    uint8_t     vps_all_independent_layers_flag;                                        // u(1)
+    uint8_t     vps_layer_id[MAX_VPS_LAYERS];                                           // []u(6)
+    uint8_t     vps_independent_layer_flag[MAX_VPS_LAYERS];                             // []u(1)
+    uint8_t     vps_max_tid_ref_present_flag[MAX_VPS_LAYERS];                           // []u(1)
+    uint8_t     vps_direct_ref_layer_flag[MAX_VPS_LAYERS][MAX_VPS_LAYERS];              // [][]u(1)
+    uint8_t     vps_max_tid_il_ref_pics_plus1[MAX_VPS_LAYERS][MAX_VPS_LAYERS];          // [][]u(3)
+    uint8_t     vps_each_layer_is_an_ols_flag;                                          // u(1)
+    uint8_t     vps_ols_mode_idc;                                                       // u(2)
+    uint8_t     vps_num_output_layer_sets_minus2;                                       // u(8)
+    uint8_t     vps_ols_output_layer_flag[MAX_VPS_OUTPUT_LAYER][MAX_VPS_OUTPUT_LAYER];  // [][]u(1)
+    uint8_t     vps_num_ptls_minus1;                                                    // u(8)
+    uint8_t     vps_pt_present_flag[MAX_VPS_PTLS];                                      // u(1)
+    uint8_t     vps_ptl_max_tid[MAX_VPS_PTLS];                                          // u(3)
+    uint8_t     vps_ols_ptl_idx[MAX_VPS_OUTPUT_LAYER];                                  // u(8)
+    uint8_t     vps_num_dpb_params_minus1;                                              // ue(v)
+    uint8_t     vps_sublayer_dpb_params_present_flag;                                   // u(1)
+    uint8_t     vps_dpb_max_tid[MAX_VPS_DPB_PARAMS];                                    // u(3)
+    uint16_t    vps_ols_dpb_pic_width[MAX_VPS_DPB_PARAMS];                              // ue(v)
+    uint16_t    vps_ols_dpb_pic_height[MAX_VPS_DPB_PARAMS];                             // ue(v)
+    uint8_t     vps_ols_dpb_chroma_format[MAX_VPS_DPB_PARAMS];                          // u(2)
+    uint8_t     vps_ols_dpb_bitdepth_minus8[MAX_VPS_DPB_PARAMS];                        // ue(v)
+    uint8_t     vps_ols_dpb_params_idx[MAX_VPS_DPB_PARAMS];                             // ue(v)
+    uint8_t     vps_timing_hrd_params_present_flag;                                     // u(1)
+    uint8_t     vps_sublayer_cpb_params_present_flag;                                   // u(1)
+    uint8_t     vps_num_ols_timing_hrd_params_minus1;                                   // ue(v)
+    uint8_t     vps_hrd_max_tid[MAX_VPS_DPB_PARAMS];                                    // u(3)
+    uint8_t     vps_ols_timing_hrd_idx[MAX_VPS_OUTPUT_LAYER];                           // ue(v)
+} vps_t;
+
 typedef struct _codec_t
 {
     param_t                 params;
@@ -119,6 +175,7 @@ typedef struct _codec_t
     intptr_t                m_frames_strd;
     bs_t                    bs;
     nal_unit_header_t       nalu_header;
+    vps_t                   vps;
 } codec_t;
 
 /*****************************************************************************
@@ -130,15 +187,46 @@ void bsInit(bs_t *bs, uint8_t *buf, intptr_t size)
     assert(!((uint32_t)buf & 7) && "bitstream must be aligned to 64 bits");
 
     bs->dwCache     = BSWAP64(AV64(buf));
-    bs->cachedBits = 8*sizeof(uint64_t);
+    bs->cachedBits  = 8*sizeof(uint64_t);
     bs->pucBuf      = buf + sizeof(uint64_t);;
-    bs->pucBufEnd   = buf + size;
+
+    // Remove trailing zero filling bytes
+    while(size > 0 && !buf[size-1])
+        size--;
+
+    bs->rbspBits = (size << 3) - 8 + (CTZ(buf[size - 1]) ^ 7);
 }
 
 uint32_t bsNextBits32(bs_t *bs)
 {
     const uint32_t bins = (uint32_t)(bs->dwCache >> 32);
     return bins;
+}
+
+uint32_t bsNextBit(bs_t *bs)
+{
+    const int shift = 64 - 1;
+    const uint32_t bins = (uint32_t)(bs->dwCache >> shift);
+    return bins;
+}
+
+void bsSkipBit(bs_t *bs)
+{
+    bs->dwCache <<= 1;
+    bs->cachedBits -= 1;
+    bs->rbspBits--;
+
+    const int left = bs->cachedBits;
+
+    // Refill
+    if(left <= 32)
+    {
+        const int shift = 64 - left - 32;
+        const uint32_t val = BSWAP32(AV32(bs->pucBuf));
+        bs->pucBuf += sizeof(uint32_t);
+        bs->cachedBits += 8*sizeof(uint32_t);
+        bs->dwCache |= ((uint64_t)val << shift);
+    }
 }
 
 uint32_t bsNextBits(bs_t *bs, int nBits)
@@ -152,9 +240,11 @@ uint32_t bsNextBits(bs_t *bs, int nBits)
 void bsSkipBits(bs_t *bs, int nBits)
 {
     assert(nBits <= 32 && "nBits must be less or equal to 32");
-    const int left = bs->cachedBits - nBits;
     bs->dwCache <<= nBits;
     bs->cachedBits -= nBits;
+    bs->rbspBits -= nBits;
+
+    const int left = bs->cachedBits;
 
     // Refill
     if(left <= 32)
@@ -171,6 +261,13 @@ uint32_t bsGetBits(bs_t *bs, int nBits)
 {
     const uint32_t bins = bsNextBits(bs, nBits);
     bsSkipBits(bs, nBits);
+    return bins;
+}
+
+uint32_t bsGetBit(bs_t *bs)
+{
+    const uint32_t bins = bsNextBit(bs);
+    bsSkipBit(bs);
     return bins;
 }
 
@@ -198,11 +295,14 @@ void bsSkipToByteAlign(bs_t *bs)
     bsSkipBits(bs, bins);
 }
 
-int bsBitsRemain(bs_t *bs)
+int bsRbspRemain(bs_t *bs)
 {
-    // NOTE: may negative number
-    const int remain = ((uint8_t*)bs->pucBufEnd - (uint8_t*)bs->pucBuf) * 8;
-    return (remain + bs->cachedBits);
+    return bs->rbspBits;
+}
+
+void bsRbspTrailingBits(bs_t *bs)
+{
+    assert(bs->rbspBits == 0 && "rbsp_trailing_bits check failed");
 }
 
 int bsFindStartCodeAndEmulation(uint8_t *inBuf, int size, uint8_t *outBuf, uint32_t *outSize)
@@ -328,12 +428,313 @@ void xDecodeInit(codec_t *codec)
     memset(codec, 0, sizeof(*codec));
 }
 
+void general_constraints_info(codec_t *codec, bs_t *bs)
+{
+    // TODO:
+}
+
+void profile_tier_level(codec_t *codec, bs_t *bs, int profileTierPresentFlag, int MaxNumSubLayersMinus1)
+{
+    int i;
+
+    if(profileTierPresentFlag)
+    {
+        int general_profile_idc = bsGetBits(bs, 7);
+        int general_tier_flag = bsGetBit(bs);
+    }
+    int general_level_idx = bsGetBits(bs, 8);
+    int ptl_frame_only_constraint_flag = bsGetBit(bs);
+    int ptl_multilayer_enabled_flag = bsGetBit(bs);
+
+    if(profileTierPresentFlag)
+    {
+        general_constraints_info(codec, bs);
+    }
+
+    int ptl_sublayer_level_present_flag[MAX_VPS_OUTPUT_LAYER];
+    for(i = MaxNumSubLayersMinus1 - 1; i >= 0; i--)
+    {
+        ptl_sublayer_level_present_flag[i] = bsGetBit(bs);
+    }
+
+    bsSkipToByteAlign(bs);
+
+    for(i = MaxNumSubLayersMinus1 - 1; i >= 0; i--)
+    {
+        if(ptl_sublayer_level_present_flag[i])
+        {
+            /*sublayer_level_idc[i] =*/ bsGetBits(bs, 8);
+        }
+    }
+    if(profileTierPresentFlag)
+    {
+        int ptl_num_sub_profiles = bsGetBits(bs, 8);
+        for(i = 0; i < ptl_num_sub_profiles; i++)
+        {
+            /*general_sub_profile_idc[i] =*/ bsGetBits(bs, 32);
+        }
+    }
+}
+
+void dpb_parameters(codec_t *codec, bs_t *bs)
+{
+    // TODO:
+}
+
+void general_timing_hrd_parameters(codec_t *codec, bs_t *bs)
+{
+    // TODO:
+}
+
+void ols_timing_hrd_parameters(codec_t *codec, bs_t *bs, int firstSubLayer, int hrd_max_tid)
+{
+    // TODO:
+}
+
+void xDecodeVPS(codec_t *codec, bs_t *bs)
+{
+    int i, j, k;
+    vps_t *const vps = &codec->vps;
+
+    memset(vps, 0, sizeof(vps_t));
+
+    //codec->vps.
+    vps->vps_video_parameter_set_id = bsGetBits(bs, 4);
+    vps->vps_max_layers_minus1 = bsGetBits(bs, 6);
+    vps->vps_max_sublayers_minus1 = bsGetBits(bs, 3);
+
+    assert(vps->vps_max_layers_minus1 + 1 <= MAX_VPS_LAYERS && "vps_max_layers_minus1 check failed");
+    assert(vps->vps_max_sublayers_minus1 + 1 <= MAX_VPS_SUB_LAYERS && "vps_max_sublayers_minus1 check failed");
+
+    if(vps->vps_max_layers_minus1 > 0)
+    {
+        if(vps->vps_max_sublayers_minus1 > 0)
+        {
+            vps->vps_default_ptl_dpb_hrd_max_tid_flag = bsGetBit(bs);
+        }
+        vps->vps_all_independent_layers_flag = bsGetBit(bs);
+    }
+
+    for(i = 0; i <= (int)vps->vps_max_layers_minus1; i++)
+    {
+        vps->vps_layer_id[i] = bsGetBits(bs, 6);
+        if(i > 0 && !vps->vps_all_independent_layers_flag)
+        {
+            vps->vps_independent_layer_flag[i] = bsGetBit(bs);
+
+            if(!vps->vps_independent_layer_flag[i])
+            {
+                uint32_t vps_max_tid_ref_present_flag;
+                vps_max_tid_ref_present_flag = vps->vps_max_tid_ref_present_flag[i] = bsGetBit(bs);
+                for(j = 0; j < i; j++)
+                {
+                    vps->vps_direct_ref_layer_flag[i][j] = bsGetBit(bs);
+                    if(vps_max_tid_ref_present_flag && vps->vps_direct_ref_layer_flag[i][j])
+                    {
+                        vps->vps_max_tid_il_ref_pics_plus1[i][j] = bsGetBits(bs, 3);
+                    }
+                }
+            }
+        }
+    }
+
+    vps->vps_ols_mode_idc = 4;
+    if(vps->vps_max_layers_minus1 > 0)
+    {
+        if(vps->vps_all_independent_layers_flag)
+        {
+            vps->vps_each_layer_is_an_ols_flag = bsGetBit(bs);
+        }
+        if(!vps->vps_each_layer_is_an_ols_flag)
+        {
+            if(!vps->vps_all_independent_layers_flag)
+            {
+                vps->vps_ols_mode_idc = bsGetBits(bs, 2);
+                assert(vps->vps_ols_mode_idc <= 2 && "vps_ols_mode_idc must be in range [0,2]\n");
+            }
+            if (vps->vps_ols_mode_idc == 2)
+            {
+                vps->vps_num_output_layer_sets_minus2 = bsGetBits(bs, 8);
+
+                for (i = 0; i <= vps->vps_num_output_layer_sets_minus2 + 1; i++)
+                {
+                    for (j = 0; j <= vps->vps_max_layers_minus1; j++)
+                    {
+                        vps->vps_ols_output_layer_flag[i][j] = bsGetBit(bs);
+                    }
+                }
+            }
+        }
+        vps->vps_num_ptls_minus1 = bsGetBits(bs, 8);
+        assert(vps->vps_num_ptls_minus1+1 <= MAX_VPS_PTLS && "vps_num_ptls_minus1 check failed");
+    }
+
+    for(i = 0; i <= vps->vps_num_ptls_minus1; i++)
+    {
+        if(i > 0)
+        {
+            vps->vps_pt_present_flag[i] = bsGetBit(bs);
+        }
+        if(!vps->vps_default_ptl_dpb_hrd_max_tid_flag)
+        {
+            vps->vps_ptl_max_tid[i] = bsGetBits(bs, 3);
+        }
+    }
+    bsSkipToByteAlign(bs);
+
+    for(i = 0; i <= vps->vps_num_ptls_minus1; i++)
+    {
+        profile_tier_level(codec, bs, vps->vps_pt_present_flag[i], vps->vps_ptl_max_tid[i]);
+    }
+
+    const int TotalNumOlss = (vps->vps_ols_mode_idc == 2) ? vps->vps_num_output_layer_sets_minus2+2
+                                                          : vps->vps_max_layers_minus1+1;
+
+    assert(TotalNumOlss < MAX_VPS_OUTPUT_LAYER && "TotalNumOlss check failed");
+
+    for(i = 0; i < TotalNumOlss; i++)
+    {
+        if(vps->vps_num_ptls_minus1 > 0 &&
+           vps->vps_num_ptls_minus1 + 1 != TotalNumOlss)
+        {
+            vps->vps_ols_ptl_idx[i] = bsGetBits(bs, 8);
+        }
+    }
+
+    if(!vps->vps_each_layer_is_an_ols_flag)
+    {
+        vps->vps_num_dpb_params_minus1 = bsGetUe(bs);
+        assert(vps->vps_num_dpb_params_minus1+1 < MAX_VPS_DPB_PARAMS && "vps_num_dpb_params_minus1 check failed");
+        const int VpsNumDpbParams = vps->vps_num_dpb_params_minus1 + 1;
+
+        if(vps->vps_max_sublayers_minus1 > 0)
+        {
+            vps->vps_sublayer_cpb_params_present_flag = bsGetBit(bs);
+        }
+
+        for(i = 0; i < VpsNumDpbParams; i++)
+        {
+            if(!vps->vps_default_ptl_dpb_hrd_max_tid_flag)
+            {
+                vps->vps_dpb_max_tid[i] = bsGetBits(bs, 3);
+            }
+            dpb_parameters(codec, bs);
+        }
+
+        // Calculate NumMultiLayerOlss
+        int NumMultiLayerOls = 0;
+        {
+            uint8_t NumLayerInOls[MAX_VPS_OUTPUT_LAYER];
+            uint8_t LayerIdInOls[MAX_VPS_OUTPUT_LAYER][MAX_VPS_OUTPUT_LAYER];
+
+            NumLayerInOls[0] = 1;
+            LayerIdInOls[0][0] = vps->vps_layer_id[0];
+            for(i = 1; i < TotalNumOlss; i++)
+            {
+                if(vps->vps_each_layer_is_an_ols_flag)
+                {
+                    NumLayerInOls[i] = 1;
+                    LayerIdInOls[i][0] = vps->vps_layer_id[i];
+                }
+                else if(vps->vps_ols_mode_idc <= 1 /*vps->vps_ols_mode_idc == 0 || vps->vps_ols_mode_idc == 1*/)
+                {
+                    NumLayerInOls[i] = i + 1;
+                    for(j = 0; j < NumLayerInOls[i]; j++)
+                    {
+                        LayerIdInOls[i][j] = vps->vps_layer_id[j];
+                    }
+                }
+                else if(vps->vps_ols_mode_idc == 2)
+                {
+                    for(j = 0, k = 0; k <= vps->vps_max_layers_minus1; k++)
+                    {
+                        // TODO: calculate layerIncludedInOlsFlag
+                        if(0 /*layerIncludedInOlsFlag[i][k]*/)
+                            LayerIdInOls[i][j++] = vps->vps_layer_id[k];
+                    }
+                    NumLayerInOls[i] = j;
+                }
+                if(NumLayerInOls[i] > 1)
+                {
+                    //MultiLayerOlsIdx[i] = NumMultiLayerOlss;
+                    NumMultiLayerOls++;
+                }
+            }
+        }
+
+        for(i = 0; i < NumMultiLayerOls; i++)
+        {
+            vps->vps_ols_dpb_pic_width[i] = bsGetUe(bs);
+            vps->vps_ols_dpb_pic_height[i] = bsGetUe(bs);
+            vps->vps_ols_dpb_chroma_format[i] = bsGetBits(bs, 2);
+            vps->vps_ols_dpb_bitdepth_minus8[i] = bsGetUe(bs);
+            if(VpsNumDpbParams > 1 && VpsNumDpbParams != NumMultiLayerOls)
+            {
+                vps->vps_ols_dpb_params_idx[i] = bsGetUe(bs);
+            }
+        }
+        vps->vps_timing_hrd_params_present_flag = bsGetBit(bs);
+        if(vps->vps_timing_hrd_params_present_flag)
+        {
+            general_timing_hrd_parameters(codec, bs);
+            if(vps->vps_max_sublayers_minus1 > 0)
+            {
+                vps->vps_sublayer_cpb_params_present_flag = bsGetBit(bs);
+            }
+            vps->vps_num_ols_timing_hrd_params_minus1 = bsGetUe(bs);
+            for(i = 0; i <= vps->vps_num_ols_timing_hrd_params_minus1; i++)
+            {
+                if(!vps->vps_default_ptl_dpb_hrd_max_tid_flag)
+                {
+                    vps->vps_hrd_max_tid[i] = bsGetBits(bs, 3);
+                    const int firstSubLayer = vps->vps_sublayer_cpb_params_present_flag ? 0 : vps->vps_hrd_max_tid[i];
+                    ols_timing_hrd_parameters(codec, bs, firstSubLayer, vps->vps_hrd_max_tid[i]);
+                }
+            }
+            if(vps->vps_num_ols_timing_hrd_params_minus1 > 0 &&
+               vps->vps_num_ols_timing_hrd_params_minus1+1 != 0)
+            {
+                for(i = 0; i < 0; i++)
+                {
+                    vps->vps_ols_timing_hrd_idx[i] = bsGetUe(bs);
+                }
+            }
+        }
+    }
+
+    const int vps_extension_flag = bsGetBit(bs);
+    if(vps_extension_flag)
+    {
+        // TODO: Skip multiple of bits at once
+        while(bsRbspRemain(bs) > 0)
+            bsSkipBit(bs);
+    }
+
+    bsRbspTrailingBits(bs);
+}
+
 int xDecodeFrame(codec_t *codec, uint8_t *buf, int size)
 {
     bs_t *bs = &codec->bs;
     bsInit(bs, buf, size);
 
     nal_unit_header(bs, &codec->nalu_header);
+
+    switch(codec->nalu_header.nal_unit_type)
+    {
+    case VPS_NUT:
+        break;
+
+    case SPS_NUT:
+        break;
+
+    case PPS_NUT:
+        break;
+
+    default:
+        fprintf(stderr, "Unsupport nal type %d\n", codec->nalu_header.nal_unit_type);
+        return -1;
+    }
 
     return 0;
 }
@@ -431,12 +832,12 @@ int main(int argc, char *argv[])
         // NALU size
         int nFrameSize = nPos1 - nPos0;
 
-        // Remove trailing zero filling bytes
-        while(!bitBuf0[nPos0 + nFrameSize - 1])
-            nFrameSize--;
-
         // Decode Frame (input NALU without Start Code)
         ret = xDecodeFrame(&codec, &bitBuf0[nPos0+4], nFrameSize);
+
+        // Check decode error
+        if(ret < 0)
+            break;
 
         // Loop Next
         nPos0 = nPos1;
